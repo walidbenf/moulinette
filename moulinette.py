69920c048c69984d9f7995de784280313d520696
walidbenf <isior@outlook.fr>
2024-10-15 00:15:26 +0200

    moulinette v013

--- moulinette.py
+++ moulinette.py
@@ -17,27 +17,65 @@ def display_differences(differences):
         print(line)
 
 def main():
-    sources1 = ['w_functions/main_w_putchar.c', 'w_functions/w_putchar.c']
-    sources2 = ['tester_functions/main_putchar.c', 'tester_functions/ft_putchar.c']
-    executable1 = 'test_w_putchar'
-    executable2 = 'test_putchar'
+    tests = [
+        {
+            "sources": ['w_functions/main_w_putchar.c', 'w_functions/w_putchar.c'],
+            "executable": 'test_w_putchar',
+            "output": None
+        },
+        {
+            "sources": ['tester_functions/main_putchar.c', 'tester_functions/ft_putchar.c'],
+            "executable": 'test_putchar',
+            "output": None
+        },
+        {
+            "sources": ['w_functions/main_w_print_alphabet.c', 'w_functions/w_putchar.c', 'w_functions/w_print_alphabet.c'],
+            "executable": 'test_w_print_alphabet',
+            "output": None
+        },
+        {
+            "sources": ['tester_functions/main_print_alphabet.c', 'tester_functions/ft_putchar.c', 'tester_functions/ft_print_alphabet.c'],
+            "executable": 'test_print_alphabet',
+            "output": None
+        },
+        {
+            "sources": ['w_functions/main_w_print_reverse_alphabet.c', 'w_functions/w_putchar.c', 'w_functions/w_print_reverse_alphabet.c'],
+            "executable": 'test_w_reverse_print_alphabet',
+            "output": None
+        },
+        {
+            "sources": ['tester_functions/main_print_reverse_alphabet.c', 'tester_functions/ft_putchar.c', 'tester_functions/ft_print_reverse_alphabet.c'],
+            "executable": 'test_reverse_print_alphabet',
+            "output": None
+        }
+    ]
     
-    # Compile the files with the different sources
-    compile_file(sources1, executable1)
-    compile_file(sources2, executable2)
-    
-    # Execute the files and capture the outputs
-    output_w_putchar = execute_file(executable1)
-    output_putchar = execute_file(executable2)
+    # Compile and execute the tests
+    for test in tests:
+        compile_file(test["sources"], test["executable"])
+        test["output"] = execute_file(test["executable"])
     
     # Compare the outputs
-    differences = compare_outputs(output_w_putchar, output_putchar)
-    
+    differences = compare_outputs(tests[0]["output"], tests[1]["output"])
     if differences:
-        print("Differences found:")
+        print("Differences found in putchar tests:")
         display_differences(differences)
     else:
-        print("No differences found.")
+        print("No differences found in putchar tests.")
+    
+    differences = compare_outputs(tests[2]["output"], tests[3]["output"])
+    if differences:
+        print("Differences found in print_alphabet tests:")
+        display_differences(differences)
+    else:
+        print("No differences found in print_alphabet tests.")
+    
+    differences = compare_outputs(tests[4]["output"], tests[5]["output"])
+    if differences:
+        print("Differences found in reverse_print_alphabet tests:")
+        display_differences(differences)
+    else:
+        print("No differences found in reverse_print_alphabet tests.")
 
 if __name__ == "__main__":
     main()
